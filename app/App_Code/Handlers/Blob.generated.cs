eb2d497d76ad437de29e84d3db2c1b8ea4847fd2
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2018-07-23 22:11:50 +0700

    Add project files.

---
+++ app/App_Code/Handlers/Blob.generated.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCompany.Handlers
+{
+    
+    
+    public partial class BlobFactoryConfig : BlobFactory
+    {
+        
+        public static void Initialize()
+        {
+            // register blob handlers
+            RegisterHandler("EmployeesPhoto", "\"dbo\".\"Employees\"", "\"Photo\"", new string[] {
+                        "\"EmployeeID\""}, "Employees Photo", "Employees", "Photo");
+        }
+    }
+}
